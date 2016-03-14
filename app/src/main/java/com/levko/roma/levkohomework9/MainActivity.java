07e8a3625c0eb729fd342814fe953e3c6d389d51
RomanLevko <pifko22@gmail.com>
2016-03-14 09:26:20 +0200

    Initial commit. Create project.

---
+++ app/src/main/java/com/levko/roma/levkohomework9/MainActivity.java
@@ -0,0 +1,13 @@
+package com.levko.roma.levkohomework9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
